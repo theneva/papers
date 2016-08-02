a28574c3e49b1cedc378367d99e719ded3d87fdc
Martin Lehmann <martin.lehmann@finn.no>
2016-08-02 10:19:50 +0200

    Fix citations

--- deployment-strategies/build.sh
+++ deployment-strategies/build.sh
@@ -8,12 +8,12 @@ COPYRIGHT_FILE_NAME='acmcopyright.sty'
 
 if [ ! -f ./$CITATION_STYLE_LANGUAGE_FILE_NAME ]; then
   echo "Downloading missing citation style language (CSL) file..."
-  curl https://raw.githubusercontent.com/citation-style-language/styles/master/chicago-author-date.csl --create-dirs -o $CITATION_STYLE_LANGUAGE_FILE_NAME
+  curl https://raw.githubusercontent.com/citation-style-language/styles/master/acm-sig-proceedings.csl --create-dirs -o $CITATION_STYLE_LANGUAGE_FILE_NAME
 fi
 
 if [ ! -f $STYLE_FILE_NAME ]; then
   echo "Downloading missing style template..."
-  curl http://www.acm.org/binaries/content/assets/publications/article-templates/sig-alternate-05-2015.cls -o $STYLE_FILE_NAME
+  curl https://www.acm.org/binaries/content/assets/publications/article-templates/sig-alternate-05-2015.cls -o $STYLE_FILE_NAME
 fi
 
 if [ ! -f $COPYRIGHT_FILE_NAME ]; then
@@ -39,4 +39,5 @@ pandoc \
   src/background.md \
   src/method-and-results.md \
   src/discussion.md \
-  src/conclusion.md
+  src/conclusion.md \
+  src/references.md
